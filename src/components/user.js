0e4b8f11140524b5900870f7469d929a9a101064
Yohann Philippot <philippot.yohann@gmail.com>
2021-03-19 18:16:58 +0100

    style: restyling user profile page

--- src/components/user.js
+++ src/components/user.js
@@ -74,8 +74,8 @@ class User extends Component{
         let deleteButton
         let modifyButton
         if(this.state.user.id == userId ){
-            deleteButton = <button className='col-8 offset-2 my-2 bg-button rounded' onClick={this.handleDelete}> Supprimer le compte </button>
-            modifyButton = <button className='col-8 offset-2 my-2 bg-button rounded' onClick={this.handleModify}> Modifier le profil </button>
+            deleteButton = <button className='col-8 offset-2 my-2 bg-delete-button' onClick={this.handleDelete}> Supprimer le compte </button>
+            modifyButton = <button className='col-8 offset-2 my-2 bg-post-button' onClick={this.handleModify}> Modifier le profil </button>
         } else {
             deleteButton= null
             modifyButton= null
@@ -83,7 +83,7 @@ class User extends Component{
 
         return(
             <div>
-                <Navbar bg="dark" variant="dark" expand="lg" sticky="top">
+                <Navbar className='header py-3' expand="lg" sticky="top">
                     <Navbar.Brand href="/posts/allPosts"><img src={logo} alt='logo groupomania'/></Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
@@ -93,21 +93,24 @@ class User extends Component{
                         </Nav> 
                     </Navbar.Collapse>
                 </Navbar>
-                <h1 className='col col-lg-4 offset-lg-4 text-center' >Mon Compte</h1>
-                <div className='mb-3 py-3 col-10 col-lg-4 offset-1 offset-lg-4 bg-login'>
-                    <div className='col my-3'>
-                        Nom : {this.state.user.lastName}
+                <div className='loginpage'>
+                    <h1 className='col col-lg-4 offset-lg-4 text-center pt-4 mb-3' >Mon Compte</h1>
+                    <div className='mb-3 py-3 col-10 col-lg-4 offset-1 offset-lg-4 bg-login radius2'>
+                        <div className='col my-3'>
+                            <span className='bold'>Nom :</span> {this.state.user.lastName}
+                        </div>
+                        <div className='col my-3'>
+                            <span className='bold'>Prénom :</span> {this.state.user.firstName}
+                        </div>
+                        <div className='col my-3'>
+                            <span className='bold'>Adresse mail :</span> {this.state.user.email}
+                        </div>
+                        {modifyButton}
+                        <br/>
+                        {deleteButton}
                     </div>
-                    <div className='col my-3'>
-                        Prénom: {this.state.user.firstName}
-                    </div>
-                    <div className='col my-3'>
-                        Adresse mail: {this.state.user.email}
-                    </div>
-                    {modifyButton}
-                    <br/>
-                    {deleteButton}
                 </div>
+                
                 
             </div>
         )
